3c100a49d36b9b6aa423439e7088e4c21d196c92
Josh Morris <jmo0624@gmail.com>
2020-01-09 01:44:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-    sql <<-SQL
+    sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?
       LIMIT 1
@@ -37,7 +37,7 @@ class Student
   end
   
   def self.count_All_students_in_grade_9
-    sql <<-SQL
+    sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 9
     SQL
